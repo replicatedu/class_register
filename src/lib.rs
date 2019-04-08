010267dddc014cb05c9824f99f293afa0a6557d3
asd <asd>
2019-04-08 01:01:47 +0000

    mostely working

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,7 @@ use std::process::{Command};
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
-//returns a command setup ready to run the tests
-fn command_wrapper(test_command: &str, command_directory: &str) -> Command {
-    let mut command = if cfg!(target_os = "windows") {
-        {
-            let mut c = Command::new("cmd");
-            c.args(&["/C", test_command]);
-            c
-        }
-    } else {
-        {
-            let mut c = Command::new("sh");
-            c.arg("-c");
-            c.arg(test_command);
-            c
-        }
-    };
-    command.current_dir(command_directory);
-    command
-}
-
-
+use git_wrapper;
 
 //holds data for instructor and students
 pub struct ClassRegister {
@@ -50,49 +30,33 @@ impl ClassRegister {
     }
 
     pub fn create_repo( &self){
-        //curl --url url -K- <<< "--user user:password"
-        let mut command = String::new();
-        command.push_str("curl --url https://api.github.com/user/repos ");
-        command.push_str(&format!("-d '{{\"name\":\"{}\",\"private\":true}}' ", 
-                         &self.repo_name));
-        command.push_str(&format!("--user \"{}:{}\"", &self.username,&self.password));
-        println!("{}", command);
-
-        let mut c = command_wrapper(&command, "/tmp/");
-        println!("{}",String::from_utf8_lossy(&c.output().unwrap().stdout));
+        git_wrapper::create_repo(&self.username, 
+                                 &self.password, 
+                                 &self.repo_name, 
+                                 "/tmp/");
     }
 
-    // git clone --bare https://github.com/exampleuser/public-repo.git
-    // cd public-repo.git
-    // git push --mirror https://github.com/yourname/private-repo.git
-    // cd ..
-    // rm -rf public-repo.git
     pub fn clone_repo_to_private( &self){
-        let mut command = String::new();
-        command.push_str(&format!("rm -rf {}.git && ", &self.repo_name));
-        command.push_str(&format!("git clone --bare {} && ",&self.class_repo_address));
-        command.push_str(&format!("cd {}.git && ", &self.repo_name));
-        command.push_str(&format!("git push --mirror https://{}:{}@github.com/{}/{}.git && ", 
-                         &self.username,
-                         &self.password,
-                         &self.username,
-                         &self.repo_name));
-        command.push_str("cd .. && ");
-        command.push_str(&format!("rm -rf {}.git", &self.repo_name));
-        println!("{}", command);
-
-        let mut c = command_wrapper(&command, "/tmp/");
-        let output = &c.output().unwrap();
-        println!("{}",String::from_utf8_lossy(&output.stdout));
-        println!("{}",String::from_utf8_lossy(&output.stderr));
+        git_wrapper::clone_class_repo_to_private(&self.username, 
+                                                 &self.password, 
+                                                 &self.repo_name, 
+                                                 "/tmp/", 
+                                                 &self.class_repo_address);
+    }
+    pub fn clone_repo_to_dir( &self ){
+        git_wrapper::clone_repo_to_dir(&self.username, 
+                                       &self.password, 
+                                       &self.repo_name, 
+                                       "/tmp/");
     }
 
-    //update from old repo
-    // cd private-repo
-    // git remote add public https://github.com/exampleuser/public-repo.git
-    // git pull public master # Creates a merge commit
-    // git push origin master
-
+    pub fn add_deploy_key( &self, key: &str){
+        git_wrapper::add_deploy_key(&self.username, 
+                                    &self.password,
+                                    &self.repo_name,
+                                    "/tmp/",
+                                    key);
+    }
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,95 @@
-fn main (){
+use std::str;
 
+use std::fs::File;
+use std::fs::OpenOptions;
+
+use std::io::prelude::*;
+use std::env;
+
+use class_crypto::ClassCrypto;
+use class_crypto::serialization::Participant;
+use class_register::ClassRegister;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
+fn read_file_to_string(filepath:&str) -> String {
+    let mut file = File::open(filepath).expect("key not there");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("error reading key");
+    contents
+}
+
+fn write_file(filepath: &str, contents: &str) {
+    match OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(filepath)
+    {
+        Ok(ref mut file) => {
+            file.set_len(0);
+            writeln!(file, "{}",contents).unwrap();
+        }
+        Err(err) => {
+            panic!("Failed to open log file: {}", err);
+        }
+    }
+}
+
+fn parse_key_mat(repo_name: &str, coord:&ClassCrypto) -> String{
+    let mut database_pub_path = "".to_string();
+    database_pub_path.push_str(&format!("/tmp/{}/database_keys.toml",repo_name));
+    let mut database_priv_path = "".to_string();
+    database_priv_path.push_str(&format!("/tmp/{}/database_keys_priv.toml",repo_name));
+    let mut deploy_key_path = "".to_string();
+    deploy_key_path.push_str(&format!("/tmp/{}/deploy_key.toml",repo_name));
+
+    let deploy_key_enc = read_file_to_string(&deploy_key_path);
+    let database_priv_key_enc = read_file_to_string(&database_priv_path);
+    let database_pub_key_enc = read_file_to_string(&deploy_key_path);
+
+    let deploy_key = coord.decrypt_from_toml(&deploy_key_enc)
+                          .expect("error decrypting deploy");
+    let database_priv_key = coord.decrypt_from_toml(&database_priv_key_enc)
+                                 .expect("error decrypting deploy");
+    let database_pub_key = coord.decrypt_from_toml(&database_pub_key_enc)
+                                .expect("error decrypting deploy");
+
+    write_file("/tmp/id_rsa", str::from_utf8(&database_priv_key).unwrap());
+    write_file("/tmp/id_rsa.pub",str::from_utf8(&database_pub_key).unwrap());
+    
+    str::from_utf8(&deploy_key).unwrap().to_string()
+   
+}
+
+fn main (){
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        panic!("args: reposity_url public_key");
+    }
+    let class_repo = &args[1];
+    let sk = &args[2];
+    let repo_name = "student_test_class";
+    let username = "hortinstein";
+    let path = "/tmp/";
+    let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
+    let mut repo_path: String = path.to_owned();
+    repo_path.push_str(&format!("/{}",repo_name));
+
+    let register = ClassRegister::new(repo_name.to_string(),
+                                      class_repo.to_string(),
+                                      username.to_string(),
+                                      password.to_string());
+
+    register.create_repo();
+    register.clone_repo_to_private();
+    register.clone_repo_to_dir();
+    let coord_key = ClassCrypto::new_from_sk("coord", sk.to_string(), false).expect("crypto failed");
+     
+    let deploy_key = parse_key_mat(repo_name,&coord_key);
+    register.add_deploy_key(&deploy_key);
 }
