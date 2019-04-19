94ecc501596659da7995f4a49c1a13060b59ca64
Alex Hortin <hortinstein@gmail.com>
2019-04-19 21:50:37 +0000

    done for the day

--- src/lib.rs
+++ src/lib.rs
@@ -1,83 +1,159 @@
+pub mod register;
+
+use std::str;
+
 use std::fs;
+use std::fs::File;
 use std::fs::OpenOptions;
-use std::io;
 
-use std::process::{Command};
+use std::io::prelude::*;
+use std::env;
 
-use std::time::{SystemTime, UNIX_EPOCH};
+use class_crypto::ClassCrypto;
+use class_crypto::serialization::Participant;
+use class_crypto::participant_to_str;
 
-use git_wrapper;
+use class_crypto::convert_me_to_serializable;
+use register::ClassRegister;
 
-//holds data for instructor and students
-pub struct ClassRegister {
-	repo_name: String,
-    class_repo_address: String,
-    username: String,
-    password: String
+use issue_database::ClassIssues;
+
+use term_painter::Color::*;
+use term_painter::ToStyle;
+use gag::Gag;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate toml;
+
+fn read_file_to_string(filepath:&str) -> String {
+    let mut file = File::open(filepath).expect("key not there");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("error reading key");
+    contents
 }
 
-impl ClassRegister {
-    pub fn new( repo_name: String,
-                class_repo_address: String, 
-                username: String, 
-                password: String) -> ClassRegister {
-        ClassRegister {
-            repo_name,
-            class_repo_address,
-            username,
-            password
+fn write_file(filepath: &str, contents: &str) {
+    match OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(true)
+        .open(filepath)
+    {
+        Ok(ref mut file) => {
+            file.set_len(0);
+            writeln!(file, "{}",contents).unwrap();
+        }
+        Err(err) => {
+            panic!("Failed to open log file: {}", err);
         }
     }
-
-    pub fn create_repo( &self){
-        git_wrapper::create_repo(&self.username, 
-                                 &self.password, 
-                                 &self.repo_name, 
-                                 "/tmp/");
-    }
-
-    pub fn clone_repo_to_private( &self){
-        git_wrapper::clone_class_repo_to_private(&self.username, 
-                                                 &self.password, 
-                                                 &self.repo_name, 
-                                                 "/tmp/", 
-                                                 &self.class_repo_address);
-    }
-    pub fn clone_repo_to_dir( &self ){
-        git_wrapper::clone_repo_to_dir(&self.username, 
-                                       &self.password, 
-                                       &self.repo_name, 
-                                       "/tmp/");
-    }
-
-    pub fn add_deploy_key( &self, key: &str){
-        git_wrapper::add_deploy_key(&self.username, 
-                                    &self.password,
-                                    &self.repo_name,
-                                    "/tmp/",
-                                    key);
-    }
-    pub fn add_file( &self, filename:&str,repo_path:&str){
-        git_wrapper::pass_add_file( filename, 
-                                    repo_path,
-                                    &self.username, 
-                                    &self.password,
-                                    &self.repo_name);
-    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn create_repo() {
+fn parse_key_mat(repo_name: &str, coord:&ClassCrypto) -> String{
+    let mut deploy_key_path = "".to_string();
+    deploy_key_path.push_str(&format!("/tmp/{}/deploy_key.toml",repo_name));
 
-        let gh = ClassRegister::new("test_class".to_string(),
-                                    "https://github.com/replicatedu/test_class".to_string(),
-                                    "hortinstein".to_string(),
-                                    "ccccc".to_string());
+    let deploy_key_enc = read_file_to_string(&deploy_key_path);
 
-        gh.create_repo();
-        gh.clone_repo_to_private();
+    let deploy_key = coord.decrypt_from_toml(&deploy_key_enc)
+                          .expect("error decrypting deploy");
+
+    str::from_utf8(&deploy_key).unwrap().to_string()
+
+}
+
+pub fn main_register (){
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        panic!("args: --create reposity_url repo_name public_key");
     }
+    let class_repo = &args[1];
+    let repo_name = &args[2];
+    let sk = &args[3];
+
+    let path = "/tmp/";
+
+
+    let username = &env::var("GITHUB_USERNAME").expect("set the GITHUB_USERNAME env");
+    let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
+    let mut repo_path: String = path.to_owned();
+    repo_path.push_str(&format!("/{}",repo_name));
+
+    let register = ClassRegister::new(repo_name.to_string(),
+                                      class_repo.to_string(),
+                                      username.to_string(),
+                                      password.to_string());
+
+    /////////////////////////////////////////////////////////////////
+    println!("{}", Yellow.paint("creating student crypto: "));
+    let mut print_gag = Gag::stdout().unwrap();
+
+    let student_crypto = ClassCrypto::new(username, false);
+    let studend_crypto_toml = participant_to_str( convert_me_to_serializable(&student_crypto));
+
+    drop(print_gag);
+    println!("{}", Green.paint("\tdone"));
+
+    /////////////////////////////////////////////////////////////////
+    println!("{}", Yellow.paint("creating student private repo: "));
+    let mut print_gag = Gag::stdout().unwrap();
+
+    register.create_repo();
+    register.clone_repo_to_private();
+    register.clone_repo_to_dir();
+
+    drop(print_gag);
+    println!("{}", Green.paint("\tdone"));
+
+    /////////////////////////////////////////////////////////////////
+    println!("{}", Yellow.paint("adding crypto to repo: "));
+    let mut print_gag = Gag::stdout().unwrap();
+
+    write_file(&(repo_path.clone()+&"/my_crypto.toml".to_owned()), &studend_crypto_toml);
+    register.add_file("my_crypto.toml", &repo_path);
+
+    drop(print_gag);
+    println!("{}", Green.paint("\tdone"));
+
+    /////////////////////////////////////////////////////////////////
+    println!("{}", Yellow.paint("adding deploy key to repo: "));
+    let mut print_gag = Gag::stdout().unwrap();
+
+    let coord_key = ClassCrypto::new_from_sk("coord", sk.to_string(), false).expect("crypto failed");
+    let deploy_key = parse_key_mat(repo_name,&coord_key);
+    register.add_deploy_key(&deploy_key);
+
+    drop(print_gag);
+    println!("{}", Green.paint("\tdone"));
+
+    /////////////////////////////////////////////////////////////////
+    println!("{}", Yellow.paint("registering to class database: "));
+    //let mut print_gag = Gag::stdout().unwrap();
+
+    let class_api = fs::read_to_string(&(repo_path.clone()+&"/api_addr".to_owned()))
+        .expect("Something went wrong reading the api_addr");
+    dbg!(&class_api);
+    //"https://api.github.com/repos/replicatedu/issue_database"
+    let issue = ClassIssues::new(class_api,
+                                 username.to_string(),
+                                 password.to_string());
+
+    let mut my_repo: String = "".to_owned();
+    //https://github.com/hortinstein/testme.git
+    my_repo.push_str(&format!(
+        "https://github.com/{}/{}.git",
+        username, repo_name
+    ));
+
+    //encrypt the repo for registry
+    let enc_my_repo = student_crypto.encrypt_to_toml(my_repo.as_bytes().to_vec(), coord_key.return_pk());
+    issue.register(&enc_my_repo);
+
+    //drop(print_gag);
+    println!("{}", Green.paint("\tdone"));
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,23 @@
-use std::str;
 
-use std::fs;
-use std::fs::File;
-use std::fs::OpenOptions;
-
-use std::io::prelude::*;
+use replicatedu_student::main_register;
 use std::env;
-
-use class_crypto::ClassCrypto;
-use class_crypto::serialization::Participant;
-use class_crypto::participant_to_str;
-
-use class_crypto::convert_me_to_serializable;
-use class_register::ClassRegister;
-
-use issue_database::ClassIssues;
-
-use term_painter::Color::*;
-use term_painter::ToStyle;
-use gag::Gag;
-
-#[macro_use]
-extern crate serde_derive;
-extern crate toml;
-
-fn read_file_to_string(filepath:&str) -> String {
-    let mut file = File::open(filepath).expect("key not there");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("error reading key");
-    contents
-}
-
-fn write_file(filepath: &str, contents: &str) {
-    match OpenOptions::new()
-        .create(true)
-        .write(true)
-        .append(true)
-        .open(filepath)
-    {
-        Ok(ref mut file) => {
-            file.set_len(0);
-            writeln!(file, "{}",contents).unwrap();
-        }
-        Err(err) => {
-            panic!("Failed to open log file: {}", err);
-        }
-    }
-}
-
-fn parse_key_mat(repo_name: &str, coord:&ClassCrypto) -> String{
-    let mut deploy_key_path = "".to_string();
-    deploy_key_path.push_str(&format!("/tmp/{}/deploy_key.toml",repo_name));
-
-    let deploy_key_enc = read_file_to_string(&deploy_key_path);
-    
-    let deploy_key = coord.decrypt_from_toml(&deploy_key_enc)
-                          .expect("error decrypting deploy");
-    
-    str::from_utf8(&deploy_key).unwrap().to_string()
-   
-}
-
-fn main (){
+use std::thread;
+fn main() {
     let args: Vec<String> = env::args().collect();
-
-    if args.len() != 4 {
-        panic!("args: reposity_url repo_name public_key");
+     if args.len() < 2 {
+        panic!("args: check readme");
     }
-    let class_repo = &args[1];
-    let repo_name = &args[2];
-    let sk = &args[3];
-
-    let path = "/tmp/";
+    if args[1] == "--register" || args[1] == "-r" {
+        main_register();
+    } else if args[1] == "--grade" || args[1] == "-g" {
 
 
-    let username = &env::var("GITHUB_USERNAME").expect("set the GITHUB_USERNAME env");
-    let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
-    let mut repo_path: String = path.to_owned();
-    repo_path.push_str(&format!("/{}",repo_name));
-
-    let register = ClassRegister::new(repo_name.to_string(),
-                                      class_repo.to_string(),
-                                      username.to_string(),
-                                      password.to_string());
-
-    /////////////////////////////////////////////////////////////////
-    println!("{}", Yellow.paint("creating student crypto: "));
-    let mut print_gag = Gag::stdout().unwrap();
-    
-    let student_crypto = ClassCrypto::new(username, false);
-    let studend_crypto_toml = participant_to_str( convert_me_to_serializable(&student_crypto));
-        
-    drop(print_gag);
-    println!("{}", Green.paint("\tdone"));
-
-    /////////////////////////////////////////////////////////////////
-    println!("{}", Yellow.paint("creating student private repo: "));
-    let mut print_gag = Gag::stdout().unwrap();
-    
-    register.create_repo();
-    register.clone_repo_to_private();
-    register.clone_repo_to_dir();
-        
-    drop(print_gag);
-    println!("{}", Green.paint("\tdone"));
-
-    /////////////////////////////////////////////////////////////////
-    println!("{}", Yellow.paint("adding crypto to repo: "));
-    let mut print_gag = Gag::stdout().unwrap();
-    
-    write_file(&(repo_path.clone()+&"/my_crypto.toml".to_owned()), &studend_crypto_toml);    
-    register.add_file("my_crypto.toml", &repo_path);
-        
-    drop(print_gag);
-    println!("{}", Green.paint("\tdone"));
-
-    /////////////////////////////////////////////////////////////////
-    println!("{}", Yellow.paint("adding deploy key to repo: "));
-    let mut print_gag = Gag::stdout().unwrap();    
-
-    let coord_key = ClassCrypto::new_from_sk("coord", sk.to_string(), false).expect("crypto failed");     
-    let deploy_key = parse_key_mat(repo_name,&coord_key);
-    register.add_deploy_key(&deploy_key);
-
-    drop(print_gag);
-    println!("{}", Green.paint("\tdone"));
-
-    /////////////////////////////////////////////////////////////////
-    println!("{}", Yellow.paint("registering to class database: "));
-    //let mut print_gag = Gag::stdout().unwrap();    
-
-    let class_api = fs::read_to_string(&(repo_path.clone()+&"/api_addr".to_owned()))
-        .expect("Something went wrong reading the api_addr");
-    dbg!(&class_api);
-    //"https://api.github.com/repos/replicatedu/issue_database" 
-    let issue = ClassIssues::new(class_api,
-                                 username.to_string(),
-                                 password.to_string());
-    
-    let mut my_repo: String = "".to_owned();
-    //https://github.com/hortinstein/testme.git
-    my_repo.push_str(&format!(
-        "https://github.com/{}/{}.git",
-        username, repo_name
-    ));
-    
-    //encrypt the repo for registry
-    let enc_my_repo = student_crypto.encrypt_to_toml(my_repo.as_bytes().to_vec(), coord_key.return_pk());
-    issue.register(&enc_my_repo);
-
-    //drop(print_gag);
-    println!("{}", Green.paint("\tdone"));
+    } else if args[1] == "--check_grade" || args[1] == "-e" {
 
 
+    } else if args[1] == "--check_registration" || args[1] == "-e" {
+
+
+    }
 }
+
