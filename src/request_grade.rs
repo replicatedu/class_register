9d7dfe6a4612c05f5829200a025eecf3f51fc679
alex hortin <hortinstein@gmail.com>
2019-04-27 21:20:55 +0000

    maybe autograding works

--- src/request_grade.rs
+++ src/request_grade.rs
@@ -12,19 +12,21 @@ use term_painter::ToStyle;
 use toml;
 
 pub fn main() {
+    let args: Vec<String> = env::args().collect();
+
      if args.len() != 4 {
         panic!("args: --create my_reposity_url coord_key");
     }
+    let username = &env::var("GITHUB_USERNAME").expect("set the GITHUB_USERNAME env");
+    let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
     let my_repo = &args[2];
     let sk = &args[4];
-    let class_api = fs::read_to_string(&(repo_path.clone() + &"/api_addr".to_owned()))
+    let class_api = fs::read_to_string(&(my_repo.clone() + &"/api_addr".to_owned()))
         .expect("Something went wrong reading the api_addr");
     dbg!(&class_api);
     //"https://api.github.com/repos/replicatedu/issue_database"
     let issue = ClassIssues::new(class_api, username.to_string(), password.to_string());
 
 
-    let username = &env::var("GITHUB_USERNAME").expect("set the GITHUB_USERNAME env");
-    let password = env::var("GITHUB_PASSWORD").expect("set the GITHUB_PASSWORD env");
 
 }
